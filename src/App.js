3a816af691563249f1042070b7d4d850441fe2f9
harshavardhan <harshavardan2k17@gmail.com>
2020-09-02 15:28:49 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, Suspense, lazy } from "react";
 import SearchParams from "./SearchParam";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 //import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 import NavBar from "./NavBar";
@@ -13,16 +13,15 @@ const App = () => {
       <ThemeContext.Provider value={theme}>
         <div>
           <NavBar />
-          <header>
+          {/* <header>
             <Link to="/">Adopt Me!</Link>
-          </header>
+          </header> */}
           <Suspense fallback={<h1>loading route …</h1>}>
             <Router>
               <SearchParams path="/" />
               <Details path="/details/:id" />
             </Router>
           </Suspense>
-          ;
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
